94117a18f9b4b46c146f0514dcbaa093e2c88921
joshnah <tiendatamser296@gmail.com>
2024-10-14 12:03:19 +0200

    routing

--- datoine/src/app/app.routes.ts
+++ datoine/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductDisplayComponent } from './product-display/product-display.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'product', component: ProductDisplayComponent },
+    { path: 'auth', component: AuthPageComponent },
+    { path: 'user', component: UserInfoComponent },
+    { path: 'cart', component: CartComponent },
+];
